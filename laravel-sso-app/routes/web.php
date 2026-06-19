8ce6215bf040e02e51de02ef443257df35408450
Elmer Roncales <elmerroncales904@gmail.com>
2026-06-19 18:14:32 +0800

    Update web.php

--- laravel-sso-app/routes/web.php
+++ laravel-sso-app/routes/web.php
@@ -8,7 +8,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
     Route::get('/', [AuthController::class, 'showLogin'])->name('login');
+    Route::get('/login', [AuthController::class, 'showLogin']);
     Route::post('/login', [AuthController::class, 'login'])->name('login.store');
+});
     Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
     Route::post('/register', [AuthController::class, 'register'])->name('register.store');
     Route::get('/forgot-password', [AuthController::class, 'showForgotPassword'])->name('password.request');
